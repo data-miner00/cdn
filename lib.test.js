9755c9bdb43aee50b99f744d3ccd855e1bafdb1e
Shaun Chong <data-miner00@users.noreply.github.com>
2026-01-17 20:52:31 +0800

    feat: Add subtract method

--- lib.test.js
+++ lib.test.js
@@ -1,7 +1,11 @@
-import { add } from "./lib.js";
+import { add, subtract } from "./lib.js";
 import assert from "node:assert";
 import { test } from "node:test";
 
 test("adds two numbers", () => {
   assert(add(2, 3), 5);
+});
+
+test("subtracts two numbers", () => {
+  assert(subtract(5, 3), 2);
 });
